0a05c71b87e725e8683d0c60606edaf04952231d
hugsea <zhangqj35@163.com>
2016-11-21 00:47:23 +0800

    增加首页URL

---
+++ Eye/Eye/Constants/EYEConstantURL.swift
@@ -0,0 +1,16 @@
+//
+//  EYEConstantURL.swift
+//  Eye
+//
+//  Created by 张庆杰 on 16/11/21.
+//  Copyright © 2016年 Zhang Qingjie. All rights reserved.
+//
+
+import Foundation
+
+struct EYEConstantURL {
+    static let URL_Base_V2 = "http://baobab.kaiyanapp.com/api/v3/"
+    // 精选
+    // 参数：1._s=f08204255c7762207cb0c5f155b3b4f6 2.f=iphone 3.net=wifi 4.p_product=EYEPETIZER_IOS 5.u=f10f96912f453ec07a90ede621e53e37bccb016e 6.v=2.9.0 7.vc=1604
+    static let URL_Selected = URL_Base_V2 + "selected"
+}
